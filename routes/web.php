f46e670c825183d8b11a82889874158d310f356d
Joseph <joseph.laravel@gmail.com>
2025-07-11 19:59:08 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 
-Route::get('/', [TodoController::class, 'index'])->name('todos.index');
+// Route::get('/', [TodoController::class, 'index'])->name('todos.index');
+
+Route::get('/', [TodoController::class, function () {
+    return 'helloworld';
+}]);
 Route::post('/store', [TodoController::class, 'store'])->name('todos.store');
 Route::get('/edit/{id}', [TodoController::class, 'edit'])->name('todos.edit');
 Route::post('/update/{id}', [TodoController::class, 'update'])->name('todos.update');
